9377ad16e2ff86b0517506f71607a1685aa37087
svivek19 <svivek030503@gmail.com>
2024-03-27 18:32:31 +0530

    project added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,6 +5,10 @@ import Particle from "../components/Particle";
 // projects imports
 import Blog from "../assets/projects/blog.png";
 import ecommerce from "../assets/projects/e-commerce.png";
+import bmi from "../assets/projects/bmi.png";
+import currency from "../assets/projects/corrency.png";
+import weather from "../assets/projects/weather.png";
+import food from "../assets/projects/food.png";
 
 const Projects = () => {
   return (
@@ -18,30 +22,65 @@ const Projects = () => {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-
-          <Col md={6} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={Blog}
               isBlog={false}
               title="Blog Website"
-              description="
-              Embark on a cutting-edge blogging journey with our React-based blog app, seamlessly integrated with Tailwind CSS for a sleek and responsive design. Elevate user experience with Firebase, utilizing its robust database for efficient data management and seamless Google authentication to enhance user security. Immerse your users in a dynamic interface with eye-catching loading animations, ensuring a smooth and engaging browsing experience. Stay connected with your audience through advanced features like email notifications powered by EmailJS, keeping your users informed and engaged with the latest updates. The integration of Toasterify adds a touch of elegance to your app, providing sleek and customizable notifications that keep users in the loop without compromising on design aesthetics. T
-              "
               ghLink="https://github.com/svivek19/react-blog-app.git"
               demoLink="https://react-blog-vivek.vercel.app/"
             />
           </Col>
 
-          <Col md={6} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={ecommerce}
               isBlog={false}
-              title="Dress Shop"
-              description="
-              I am currently working on developing an e-commerce dress shop utilizing React, Tailwind CSS, and Firebase for authentication. The project aims to provide a seamless and secure shopping experience for users. Firebase Authentication is integrated to ensure a reliable and user-friendly login system. The UI is styled using Tailwind CSS, offering a responsive and visually appealing design for the dress shop. To enhance user experience, the project incorporates Toastify for displaying notifications, ensuring users are informed about their actions, such as successful logins or item additions to the cart. The shopping cart functionality is implemented to allow users to easily manage and review their selected items before proceeding to checkout. 
-              "
+              title="Ecommerce Website"
               ghLink="https://github.com/svivek19/react-ecommerce.git"
               demoLink="https://react-dress-shop-ecommerce-vvk.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={food}
+              isBlog={false}
+              title="Recipe Finder"
+              ghLink="https://github.com/svivek19/recipe-finder-react.git"
+              demoLink="https://recipe-finder-vivek.netlify.app/"
+            />
+          </Col>
+        </Row>
+
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={currency}
+              isBlog={false}
+              title="Real Time Currency Converter"
+              ghLink="https://github.com/svivek19/react-currency-converter.git"
+              demoLink="https://currency-converter-vivek.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={weather}
+              isBlog={false}
+              title="Weather app"
+              ghLink="https://github.com/svivek19/react-weather-app.git"
+              demoLink="https://react-weather-app-vivek.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bmi}
+              isBlog={false}
+              title="BMI Calculator"
+              ghLink="https://github.com/svivek19/react-bmi-calculator.git"
+              demoLink="https://bmi-calculator-vivek.netlify.app/"
             />
           </Col>
         </Row>
